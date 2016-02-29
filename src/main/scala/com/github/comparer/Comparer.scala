e595705db48e8475ce270168ab5afdf16e8b7bf7
Yuriy Ankudinov <stremlenye@gmail.com>
2016-02-29 12:42:04 +0100

    Add monaidic interface to Line

--- src/main/scala/com/github/comparer/Comparer.scala
+++ src/main/scala/com/github/comparer/Comparer.scala
@@ -7,16 +7,32 @@ import scala.language.implicitConversions
   */
 object Comparer {
 
-  abstract class Line[A](row: Int) {
-    def isEmpty
+  abstract class Line[A](row: Int) extends TraversableOnce[Line[A]]{
+    self =>
+    def isEmpty: Boolean
+    def isDefined: Boolean = !isEmpty
+    override def hasDefiniteSize: Boolean = true
+    override def foreach[U](f: (Line[A]) => U): Unit = f(self)
+    override def seq: TraversableOnce[Line[A]] = self
+    override def copyToArray[B >: Line[A]](xs: Array[B], start: Int, len: Int): Unit = Array(self, xs)
+    override def forall(p: (Line[A]) => Boolean): Boolean = p(self)
+    override def find(p: (Line[A]) => Boolean): Option[Line[A]] = if(isDefined) Some(self) else None
+    override def exists(p: (Line[A]) => Boolean): Boolean = p(self)
+    override def toTraversable: Traversable[Line[A]] = new Traversable[Line[A]] {
+      override def foreach[U](f: (Line[A]) => U): Unit = f(self)
+    }
+    override def toStream: Stream[Line[A]] = Stream(self)
+    override def toIterator: Iterator[Line[A]] = Iterator(self)
+    override def isTraversableAgain: Boolean = true
+
   }
 
   case class TextLine[A](row: Int, text: A) extends Line[A](row) {
-    override def isEmpty: Unit = false
+    override def isEmpty: Boolean = false
   }
 
   case class EmptyLine[A](row: Int) extends Line[A](row) {
-    override def isEmpty: Unit = true
+    override def isEmpty: Boolean = true
   }
 
   case class MatchLine[A](row: Int, left: Line[A], right: Line[A])
@@ -29,7 +45,8 @@ object Comparer {
       case ((l: Option[A], r: Option[A]), row) => (row, toLine(l, row), toLine(r, row))
     }).flatMap {
       case (row, l: TextLine[A], r: TextLine[A]) if l.text == r.text => List(MatchLine(row, l, r))
-      case (row, l: TextLine[A], r: TextLine[A]) if l.text != r.text => List(MatchLine(row, l, EmptyLine(row)), MatchLine(row, EmptyLine(row), r))
+      case (row, l: TextLine[A], r: TextLine[A]) if l.text != r.text =>
+        List(MatchLine(row, l, EmptyLine(row)), MatchLine(row, EmptyLine(row), r))
       case (row, l, r) => List(MatchLine(row, l, r))
     }
   }
